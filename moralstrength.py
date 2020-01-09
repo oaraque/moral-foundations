16d41f6b03d5f452de6b0b1cdc4b1ef85cf01649
Lorenzo Gatti <gattilorenz@gmail.com>
2020-01-09 12:58:23 +0100

    Implemented VADER metric. Added normalization of scores between -1 and +1  (off by default, should not break compatibility)

--- moralstrength.py
+++ moralstrength.py
@@ -2,55 +2,83 @@ import lexicon_use
 from estimators import estimate,models
 import spacy
 import en_core_web_sm
+import math
 
 nlp = en_core_web_sm.load()
 moral_options_lexicon = ['care', 'fairness', 'loyalty', 'authority', 'purity']
 moral_options_predictions = moral_options_lexicon + ['non-moral']
 
 
-def word_moral_value(word,moral):
+def word_moral_value(word,moral,normalized=False):
     """Returns the association strength between word and moral trait, 
        as rated by annotators. Value ranges from 1 to 9.
-       1: words closely associated to harm, cheating, betrayal, subversion, degradation
-       9: words closely associated to care, fairness, loyalty, authority, sanctity
-       If the word is not in the lexicon of that moral trait, returns -1.
+       1: words closely associated to vices: harm, cheating, betrayal, subversion, degradation
+       9: words closely associated to virtues: care, fairness, loyalty, authority, sanctity
+       normalized=True will normalize each annotation in the range -1 (vice) to +1 (virtue)
+       If the word is not in the lexicon of that moral trait, returns NaN.
        For a list of available traits, get_available_lexicon_traits()
        """
        
     if moral not in moral_options_lexicon:
         raise ValueError('Invalid moral trait "{}" specified. Valid traits are: {}'.format(moral,moral_options_lexicon))
-    return lexicon_use.moral_value(word=word, moral=moral)
+    moral_value = lexicon_use.moral_value(word,moral,normalized)
+    return moral_value
 
 
-def word_moral_values(word):
+def word_moral_annotations(word,normalized=False):
     """Returns a dict that gives the association strength between word and every 
        moral trait, as rated by annotators. Value ranges from 1 to 9.
        1: words closely associated to harm, cheating, betrayal, subversion, degradation
        9: words closely associated to care, fairness, loyalty, authority, purity/sanctity
-       If the word is not in the lexicon of that moral trait, returns -1."""
-       
-    return {moral: lexicon_use.moral_value(word=word, moral=moral) for moral in moral_options_lexicon}
+       normalized=True will normalize each annotation in the range -1 (vice) to +1 (virtue)
+       If the word is not in the lexicon of that moral trait, returns NaN."""
+    return {moral: lexicon_use.moral_value(word, moral,normalized) for moral in moral_options_lexicon}
 
 
-def string_average_moral(text,moral):
+def string_average_moral(text,moral,normalized=False):
     """Returns the average of the annotations for the words in the sentence (for one moral).
-       If no word is recognized/found in the lexicon, returns -1.
-       Words are lemmatized using spacy.
+       normalize=True will normalize each annotation in the range -1 (vice) to +1 (virtue)
+       If no word is recognized/found in the lexicon, returns NaN.
+       Text is lemmatized using spacy.
        """
-	sum = 0
-	recognized_words_no = 0
-	for token in nlp(text):
-		lemma = token.lemma_
-		value = word_moral_value(lemma,moral)
-		#print("lemma: {}".format(lemma))
-		if value>-1:
-			sum += value
-			recognized_words_no += 1
-	if recognized_words_no == 0:
-		return -1
-	else:
-		return sum/recognized_words_no
-	
+    sum = 0
+    import pdb
+    pdb.set_trace()
+    recognized_words_no = 0
+    for token in nlp(text):
+        lemma = token.lemma_
+        value = word_moral_value(lemma,moral,normalized)
+        #print("lemma: {}".format(lemma))
+        if not math.isnan(value):
+            sum += value
+            recognized_words_no += 1
+    if recognized_words_no == 0:
+        return float('NaN')
+    else:
+        return sum/recognized_words_no
+
+
+
+def string_vader_moral(text,moral,alpha=15):
+    """Returns a normalized annotation score for the words in the sentence (for one moral).
+       Score ranges from -1 (vices) to 1 (virtues) and is calculated as in VADER (Hutto and Gilbert, 2014).
+       The optional alpha parameter defines how quickly the score maxes out (to 1 or -1), and can be tweaked (e.g. for very long texts it *might* be more sensible to set it to 150; 15 is the default taken from VADER).
+       If no word is recognized/found in the lexicon, returns NaN.
+       Text is lemmatized using spacy.
+       """
+    score = 0
+    recognized_words_no = 0
+    for token in nlp(text):
+        lemma = token.lemma_
+        value = word_moral_value(lemma,moral,normalized=True)
+        if not math.isnan(value):
+            score += value
+            recognized_words_no += 1
+    if recognized_words_no == 0:
+        return float('NaN')
+    else:
+        return score/math.sqrt((score*score) + alpha)
+
 
 
 def get_available_models():
